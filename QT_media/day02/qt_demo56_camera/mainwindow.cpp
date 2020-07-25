3a8f6f76bacf783c2d5ee9f8caf834c265b459a6
hewei-bit <1003826976@qq.com>
2020-07-25 14:28:01 +0800

    QT-音视频-调用Pc摄像头

--- QT_media/day02/qt_demo56_camera/mainwindow.cpp
+++ QT_media/day02/qt_demo56_camera/mainwindow.cpp
@@ -24,39 +24,39 @@ MainWindow::~MainWindow()
 void MainWindow::on_startBtn_clicked()
 {
     //QCamera can be used with QCameraViewfinder for viewfinder display
-        camera = new  QCamera(ui->comboBox->currentText().toUtf8(),this);
+    camera = new  QCamera(ui->comboBox->currentText().toUtf8(),this);
 
-       // viewfinder = new QCameraViewfinder(); //它间接继承了QWidget，如果不设置父窗口，则
-                                             //它做为一个独立的窗口显示
-         viewfinder = new QCameraViewfinder(ui->widget);
-         //设置显示大小跟父控件一样大
-         viewfinder->resize(ui->widget->size());
-        viewfinder->show();
+    // viewfinder = new QCameraViewfinder(); //它间接继承了QWidget，如果不设置父窗口，则
+                                     //它做为一个独立的窗口显示
+    viewfinder = new QCameraViewfinder(ui->widget);
+    //设置显示大小跟父控件一样大
+    viewfinder->resize(ui->widget->size());
+    viewfinder->show();
 
-         camera->setViewfinder(viewfinder);
-            camera->setCaptureMode(QCamera::CaptureStillImage);
-         camera->start(); //设置完成后，要进行摄像头，否则，摄像头不会工作
+    camera->setViewfinder(viewfinder);
+    camera->setCaptureMode(QCamera::CaptureStillImage);
+    camera->start(); //设置完成后，要进行摄像头，否则，摄像头不会工作
 
 
-         imageCapture = new QCameraImageCapture(camera);
+    imageCapture = new QCameraImageCapture(camera);
 
-         //当它抓拍到一帧数据时，会发送一个信号
-        //void QCameraImageCapture::imageCaptured(int id, const QImage &preview)
-         connect(imageCapture,&QCameraImageCapture::imageCaptured,
-                 this,&MainWindow::save_photo);
+    //当它抓拍到一帧数据时，会发送一个信号
+    //void QCameraImageCapture::imageCaptured(int id, const QImage &preview)
+    connect(imageCapture,&QCameraImageCapture::imageCaptured,
+         this,&MainWindow::save_photo);
 
 }
 
 void MainWindow::save_photo(int id, const QImage &preview)
 {
-        qDebug() << id;
+    qDebug() << id;
 
-        QPixmap map = QPixmap::fromImage(preview);
-       map = map.scaled(ui->label->size());
-        ui->label->setPixmap(map);
+    QPixmap map = QPixmap::fromImage(preview);
+    map = map.scaled(ui->label->size());
+    ui->label->setPixmap(map);
 }
 
 void MainWindow::on_captureBtn_clicked()
 {
-        imageCapture->capture();
+    imageCapture->capture();
 }
